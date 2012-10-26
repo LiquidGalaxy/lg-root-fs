54145910c4fd88514b4c961e8e824669a2fffb40
root <root@lgX.liquid.local>
2012-10-26 21:49:30 +0000

    merged screensaver updates and dependancies (power/sleep management) with screensaver augmentation to send stats active/idle to statsd on relative head node

--- home/lg/bin/screensaver.py
+++ home/lg/bin/screensaver.py
@@ -16,67 +16,116 @@
 """
 Executes applied command periodically while Space Navigator isn't touched.
 It can be used to add screensaver-ish application to Liquid Galaxy.
+Can also use dpms to put displays to sleep after a longer period of neglect.
 """
 
 import fcntl
 import os
 import sys
 import time
+import datetime
+import re
+import statsd
+from statsd import StatsdTimer
+
+if os.path.isfile('/etc/debian_chroot'):
+  myname = open("/etc/debian_chroot","r").read()
+  myname = myname.rstrip('\n')
+else:
+  myname = os.uname()[1]
+
+statsd.init_statsd({'STATSD_HOST': 'lg-head', 'STATSD_BUCKET_PREFIX': "%s" % myname}
+timer = statsd.StatsdTimer('usage')
+timer.start()
 
 # Config
-tour_check_per = 30
-tour_wait_for_trigger = 4
+wake_check_per = 2
+tour_check_per = 40
+tour_wait_for_trigger = 2
+sleep_wait_for_trigger = 40
 # Input Devices
 space_nav = "/dev/input/spacenavigator"
 quanta_ts = "/dev/input/quanta_touch"
+#Time Range Data
+time_now = datetime.datetime.now()
+#OS variables
+sleep_range =  os.environ.get('LG_SLEEP_TIME')
+sleep_override = os.environ.get('LG_SLEEP_WAKE')
 
-def Touched(dev1, dev2):
+
+# Commands
+wake_cmd = ">/dev/null /home/lg/bin/lg-run-bg /usr/bin/xset -display :0 dpms force on"
+sleep_cmd = ">/dev/null /home/lg/bin/lg-run-bg /usr/bin/xset -display :0 dpms force standby"
+
+
+##
+def configure_range( ranger):
+  s_time_raw, e_time_raw = ranger.split('-', -1)
+  s_time_hour, s_time_min = s_time_raw.split(':', -1)
+  e_time_hour, e_time_min = e_time_raw.split(':', -1)
+  s_time = datetime.datetime(time_now.year, time_now.month, time_now.day, int(s_time_hour), int(s_time_min), 00)
+  e_time = datetime.datetime(time_now.year, time_now.month, time_now.day+1 , int(e_time_hour), int(e_time_min), 00)
+return#
+
+def check_range():
+  configure_range(sleep_range)
+  if time_now > s_time and time_now < e_time:
+    print "within range"
+    return True
+  else:
+    print "outside of range"
+    return False
+
+##
+def Touched(dev1, dev2, runmode):
   # open file handles first
-  devlist = []
-  for dev in [dev1, dev2]:
-    try:
-      a = open(dev)
-    except:
-      print "Failed to open {0}".format( dev )
-    else:
-      devlist.append(a)
-
-  for dev in devlist:
-    fd = dev.fileno()
-    fcntl.fcntl(fd, fcntl.F_SETFL, os.O_NONBLOCK)
-
-  # then sleep and let input accumulate
-  time.sleep(tour_check_per)
-
-  # then read input data, if any
-  for dev in devlist:
-    try:
-      dev.read(6000)
-      return True
-    except IOError:
-      pass
-
-  # default to return False if no USB devs Touched
-  return False
+  with open(dev1) as a, open(dev2) as b:
+    for dev in [a, b]:
+      fd = dev.fileno()
+      fcntl.fcntl(fd, fcntl.F_SETFL, os.O_NONBLOCK)
+    # then sleep and let input accumulate
+    if   runmode == 1:
+      time.sleep(tour_check_per)
+    elif runmode == 2:
+      time.sleep(wake_check_per)
+    # then read input data, if any
+    for dev in [a, b]:
+      try:
+        dev.read(6000)
+        return True
+      except IOError:
+        pass
+    # default to return False if no USB devs Touched
+    return False
 
 def main():
   if len(sys.argv) != 2:
     print "Usage:", sys.argv[0], "<command>"
     print "<command> will be called every", tour_check_per, "seconds",
-    print "after", (tour_check_per * tour_wait_for_trigger), "seconds"
+    print "after", (tour_check_per * wait_for_trigger), "seconds"
     print "if USB devs are not touched."
     sys.exit(1)
-  
+
   cmd = sys.argv[1]
+  runmode = 1 # 1 = tour, 2 = displaysleep
   cnt = 0
   while True:
-    if Touched(space_nav, quanta_ts):
+    if Touched(space_nav, quanta_ts, runmode):
       cnt = 0
       print "Touched."
+      timer.split('active')
+      if runmode == 2:
+        os.system(wake_cmd)
+      runmode = 1
     else:
       cnt += 1
-      if cnt >= tour_wait_for_trigger:
+      if cnt >= sleep_wait_for_trigger and check_range() == True:
+        runmode = 2
+        print sleep_cmd
+        os.system(sleep_cmd)
+      elif cnt >= tour_wait_for_trigger and check_range() == False:
         print cmd
+        timer.split('idle')
         os.system(cmd)
       else:
         print "Wait...", cnt
