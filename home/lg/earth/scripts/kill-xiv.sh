a9c290542e5af5b350b6933f46a2ba3b2c7d2d4c
Matt Vollrath <matt@endpoint.com>
2013-04-19 19:46:41 -0400

    Block in kill-xiv.sh

--- home/lg/earth/scripts/kill-xiv.sh
+++ home/lg/earth/scripts/kill-xiv.sh
@@ -4,5 +4,5 @@
 
 if [[ "${LG_MASTERSLAVE[0]:-slave}" == "master" ]]; then
     # resume Earth procs while killing xiv procs
-    lg-sudo --parallel "killall -CONT googleearth-bin; killall -9 pano-launcher.sh; killall -9 xiv"
+    lg-sudo --parallel --wait --timeout 1 "killall -CONT googleearth-bin; killall -9 run-xiv.sh; killall -9 xiv"
 fi
